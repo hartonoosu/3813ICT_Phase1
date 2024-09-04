b20caa94ef59e9f13b572dd38ffd7492edb8c35a
Hartono <hartonoosu@gmail.com>
2024-09-04 19:04:13 +1000

    add some styles to edit profile components

--- chat/src/app/app.routes.ts
+++ chat/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AccountComponent } from './account/account.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ChatComponent } from './chat/chat.component';
+import { AdminComponent } from './admin/admin.component';
+import { GroupsComponent } from './groups/groups.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect root URL to /login
@@ -10,5 +13,8 @@ export const routes: Routes = [
     {path:'dashboard', component:DashboardComponent},
     {path:'account', component:AccountComponent},
     {path:'profile', component:ProfileComponent},
+    {path:'chat', component:ChatComponent},
+    {path:'admin', component: AdminComponent},
+    {path:'groups', component: GroupsComponent},
 
 ];
