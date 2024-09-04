b212117c0f85a657c0d0411e6cbd945b6d10e88d
Hartono <hartonoosu@gmail.com>
2024-09-04 18:05:03 +1000

    add basic edit profile page

--- chat/src/app/app.routes.ts
+++ chat/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AccountComponent } from './account/account.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect root URL to /login
     {path:'login', component:LoginComponent},
     {path:'dashboard', component:DashboardComponent},
     {path:'account', component:AccountComponent},
+    {path:'profile', component:ProfileComponent},
 
 ];
